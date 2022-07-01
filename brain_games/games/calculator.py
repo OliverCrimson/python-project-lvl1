888988e841ee6d53ac4034d233e61a38a609c3b4
MonkeyBusiness <crimson.oliver@gmail.com>
2022-07-01 13:40:54 +0300

    costatns removed + improved variables names in games/calc.py

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -2,19 +2,17 @@ import random
 
 
 DESCRIPT = 'What is the result of the expression?'
-MIN_NUM = 1
-MAX_NUM = 99
 
 
 def form_task_and_answer():
-    first_num = random.randint(MIN_NUM, MAX_NUM)
-    sec_num = random.randint(MIN_NUM, MAX_NUM)
+    first_num = random.randint(1, 99)
+    second_num = random.randint(1, 99)
     sign = random.choice('+-*')
-    game_task = ('{x} {y} {z}'.format(x=first_num, y=sign, z=sec_num))
+    game_task = ('{x} {y} {z}'.format(x=first_num, y=sign, z=second_num))
     if sign == '+':
-        result = first_num + sec_num
+        result = first_num + second_num
     elif sign == '-':
-        result = first_num - sec_num
+        result = first_num - second_num
     else:
-        result = first_num * sec_num
+        result = first_num * second_num
     return game_task, result
