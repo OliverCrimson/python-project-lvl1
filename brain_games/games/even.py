d89e1a78140b903ef9b8392dd83e06c9615aa6e0
MonkeyBusiness <crimson.oliver@gmail.com>
2022-07-04 13:28:53 +0300

    making better 60%

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,9 +6,9 @@ DESCRIPT = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def form_task_and_answer():
 
-    number = randint(1, 99)
-    question = str(number)
-    even_number = (number % 2 == 0)
+#    number = randint(1, 99)
+    question = str(randint(1, 99))
+    even_number = int(question) % 2 == 0
     if even_number:
         right_answer = 'yes'
     else:
