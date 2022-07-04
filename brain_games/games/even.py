1e3b8eee27e23037206c5db81333d7fa8ffe33a5
MonkeyBusiness <crimson.oliver@gmail.com>
2022-07-04 13:52:09 +0300

    solved tests not passing

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,6 +6,5 @@ DESCRIPT = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def form_task_and_answer():
     question = str(randint(1, 99))
-    even_number = int(question) % 2 == 0
     right_answer = 'no' if int(question) % 2 else 'yes'
     return question, right_answer
