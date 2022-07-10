77d706a34bb142a1a36c33956f4b77267616010a
MonkeyBusiness <crimson.oliver@gmail.com>
2022-07-10 12:34:34 +0300

    got rid of ifs in calculator

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -8,12 +8,13 @@ DESCRIPT = 'What is the result of the expression?'
 def form_task_and_answer():
     first_num = randint(1, 99)
     second_num = randint(1, 99)
-    sign = choice('+-*')
-    question = f'{first_num} {sign} {second_num}'
-    if sign == '+':
-        result = add(first_num, second_num)
-    elif sign == '-':
-        result = sub(first_num, second_num)
-    else:
-        result = mul(first_num, second_num)
+    operations = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+    }
+    operation = choice(list(operations.keys()))
+    result = str(operations[operation](first_num, second_num))
+    question = f'{first_num} {operation} {second_num}'
+
     return question, result
